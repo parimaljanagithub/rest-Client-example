b5e5f4daeeaa4a40c915840bab164d47bef124d8
parimaljanagithub <40845042+parimaljanagithub@users.noreply.github.com>
2019-04-07 19:48:16 +0530

    Update init.groovy

--- jenkins2/init.groovy
+++ jenkins2/init.groovy
@@ -1,5 +1,24 @@
+#!groovy
+import hudson.security.*
+import jenkins.model.*
+import hudson.model.*
+import hudson.security.csrf.DefaultCrumbIssuer
+import org.jenkinsci.plugins.scriptsecurity.scripts.*
+import com.cloudbees.plugins.credentials.impl.*;
+import com.cloudbees.plugins.credentials.*;
+import com.cloudbees.plugins.credentials.domains.*;
+
+
+def instance = Jenkins.getInstance()
+
+instance.setNumExecutors(5)
+
+instance.setCrumbIssuer(new DefaultCrumbIssuer(true))
+
+def hudsonRealm = new HudsonPrivateSecurityRealm(false)
+
 //Credentials
 //TODO inform your credentials here, changing user and password
-Credentials c = (Credentials) new UsernamePasswordCredentialsImpl(CredentialsScope.GLOBAL,"janaparimal.jana@gmail.com", "India@2019", System.getenv()['USER_NAME'], System.getenv()['USER_PWD'])
+Credentials c = (Credentials) new UsernamePasswordCredentialsImpl(CredentialsScope.GLOBAL,"git_openapiv2", "openapiv2","janaparimal.jana@gmail.com","India@2019")
 SystemCredentialsProvider.getInstance().getStore().addCredentials(Domain.global(), c)
 
