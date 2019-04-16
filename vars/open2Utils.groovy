aa1379519f2e5a43eab8483e1a95692222207616
parimaljanagithub <40845042+parimaljanagithub@users.noreply.github.com>
2019-04-16 15:15:45 +0530

    Update open2Utils.groovy

--- vars/open2Utils.groovy
+++ vars/open2Utils.groovy
@@ -1,4 +1,7 @@
-String getDate() { return new Date().format('yyyyMMdd') }
+String getDate() { 
+    println('heloo inside open2utils')
+    return new Date().format('yyyyMMdd')
+}
 
 String getPomVersion() {
     return readMavenPom(file: '../pom.xml').project.version
