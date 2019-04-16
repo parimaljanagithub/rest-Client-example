302c5d73d77a8bc1db2cf250be976d22ff173523
parimaljanagithub <40845042+parimaljanagithub@users.noreply.github.com>
2019-04-16 15:11:40 +0530

    Update open2Utils.groovy

--- vars/open2Utils.groovy
+++ vars/open2Utils.groovy
@@ -4,4 +4,10 @@ String getPomVersion() {
     return readMavenPom(file: '../pom.xml').project.version
 }
 
+String increaseVersion(String version) {
+    version.toInteger() + 1 
+    println(version)
+    return version
+}
+
 
