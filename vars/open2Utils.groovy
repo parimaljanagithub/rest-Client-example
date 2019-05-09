25b94f76d35cb1a6c33b48eaccb488620d14d842
parimaljanagithub <40845042+parimaljanagithub@users.noreply.github.com>
2019-05-09 10:01:27 +0530

    Update open2Utils.groovy

--- vars/open2Utils.groovy
+++ vars/open2Utils.groovy
@@ -14,3 +14,17 @@ String increaseVersion(String version) {
 }
 
 
+Boolean userInputWithComment(String title) {
+    try {
+        def userInput = input(message: title, parameters: [
+                choice(name: 'Select', choices: 'Yes\nNo', description: 'Confirm to promote the deployment to PROD?'),
+                text(name: 'Comment', defaultValue: '', description: 'Please do a comment if necessary here')])
+        echo("Comment: ${userInput.Comment}")
+        return userInput.Select == 'Yes'
+    } catch (err) { // input false
+        def user = err.getCauses()[0].getUser()
+        echo("Aborted by: [${user}]")
+        return false
+    }
+}
+
